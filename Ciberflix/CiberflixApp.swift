371e61b7f83751de8f7a1c5732e979d6ac103136
Anthony <alex.apd@hotmail.com>
2023-05-29 09:00:37 -0700

    Initial Commit

---
+++ Ciberflix/CiberflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiberflixApp.swift
+//  Ciberflix
+//
+//  Created by Anthony on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CiberflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
